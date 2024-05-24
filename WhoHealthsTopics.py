d551d0a642397bab11308727207d735de84b37a4
canerergun <canerrergunn@gmail.com>
2024-05-24 17:29:56 +0300

    Kategori ve Başlık Script Edildi!

--- WhoHealthsTopics.py
+++ WhoHealthsTopics.py
@@ -5,7 +5,12 @@ response = requests.get("https://www.who.int/health-topics")
 if response.status_code == 200:
     soup = BeautifulSoup(response.content, 'html.parser')
     basliklar = soup.find_all(class_="heading text-underline")
- 
-    for i in basliklar:
-        print(i.get_text())
-        print("-"*50)
+    kategoriler = soup.find_all(class_="date")
+
+
+    for kategori, baslik in zip(kategoriler, basliklar):
+        print(f"Kategori: {kategori.get_text().strip()}")
+        print(f"Başlık: {baslik.get_text().strip()}")
+        print("-" * 50)
+else:
+    print(f"Web sayfası alınamadı. Durum kodu: {response.status_code}")
